5f450bdc8c1e71f0864f01edb9f2a890bd57190f
Nathan Waldron <waldron.nathan@gmail.com>
2016-04-28 17:10:59 -0400

    Update ExemptServiceCalls.cs

--- Server-Side Message Inspector with Service Call Exemptions/ExemptServiceCalls.cs
+++ Server-Side Message Inspector with Service Call Exemptions/ExemptServiceCalls.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Web;
 
-namespace WCF123.ApplicationService.Host
+namespace WcfService123.ApplicationService.Host
 {
     public static class ExemptServiceCalls
     {
