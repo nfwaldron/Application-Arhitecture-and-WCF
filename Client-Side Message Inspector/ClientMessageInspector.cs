b46a9d59973d9eba3f4b81d39da63bbdb8f21d93
Nathan Waldron <waldron.nathan@gmail.com>
2016-04-28 11:47:26 -0400

    Client side message inspector
    This client side message inspector gets the user token, which is make
    available to the application using a ShareObjects class, and appends it
    to all outgoing service requests using a custom header. In the custom
    header, TokenHeader, the user's session token is added. When the request
    is received on the server side by the server message inspector in the
    WCF123 Project, the XML is parsed for the Session token, the Service,
    Service Call, and the Body.

---
+++ Client-Side Message Inspector/ClientMessageInspector.cs
@@ -0,0 +1,58 @@
+﻿using ProjectX.ApplicationService.Contracts.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ProjectX
+{
+    public class ClientMessageInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+                // Shared object that stores the user object once it's created
+		UserDataContract myUser = MySharedObjects.MyUser;
+
+            if (myUser!=null)
+            {
+                // Create the Message Header
+                MessageHeader header = MessageHeader.CreateHeader("TokenHeader", "TokenNameSpace", myUser.Token);
+
+                // Create buffered copy 
+                MessageBuffer buffer = request.CreateBufferedCopy(Int32.MaxValue);
+                request = buffer.CreateMessage();
+				
+		// Add the custom Message Header
+                request.Headers.Add(header);
+
+		//////// CUSTOM CODE FOR VIEWING THE MESSAGE ...just used to make sure that the actual request looks like you want it to ////////
+				
+                // Make a copy of the message for viewing
+                Message msgCopy = buffer.CreateMessage();
+                msgCopy.Headers.Add(header);
+
+                // Get the XML content
+                var strMessage = msgCopy.ToString();
+
+                // Get the SOAP XML body content
+                XmlDictionaryReader xrdr = msgCopy.GetReaderAtBodyContents();
+                string bodyData = xrdr.ReadOuterXml();
+
+                // Replace the body placeholder with the actual SOAP body.
+                strMessage = strMessage.Replace("... stream ...", bodyData);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Client-Side Message Inspector/ClientMessageInspectorBehaviorExtensionElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX
+{
+    class ClientMessageInspectorBehaviorExtensionElement : BehaviorExtensionElement
+    {
+        public ClientMessageInspectorBehaviorExtensionElement()
+        {
+
+        }
+
+        public override Type BehaviorType
+        {
+            get
+            {
+                return typeof(ClientMessageInspectorBehavior);
+            }
+        }
+
+        protected override object CreateBehavior()
+        {
+            return new ClientMessageInspectorBehavior();
+        }
+
+    }
+}
