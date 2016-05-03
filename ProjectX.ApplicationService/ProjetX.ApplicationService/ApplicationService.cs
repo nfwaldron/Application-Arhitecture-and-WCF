dd6344300c9b54bc01c634b0f54e039ded6063e5
Nathan Waldron <waldron.nathan@gmail.com>
2016-05-03 13:34:20 -0400

    Update

--- ProjectX.ApplicationService/ProjetX.ApplicationService/ApplicationService.cs
+++ ProjectX.ApplicationService/ProjetX.ApplicationService/ApplicationService.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjetX.ApplicationService
+namespace ProjectX.ApplicationService
 {
     /// <summary>
     /// Created By: Nathan Waldron
