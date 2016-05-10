4d76ad465be744d6ff3c8830d77fbd756d99c76a
Nathan Waldron <waldron.nathan@gmail.com>
2016-05-10 17:01:35 -0400

    Restful Services With WCF
    RESTful services are those which follow the REST (Representational State
    Transfer) architectural style. Before implementing your first RESTful
    service, lets first understand the concept behind it. As we know that
    WCF allows us to make calls and exchange messages using SOAP over a
    variety of protocols i.e. HTTP, TCP, Named Pipes and MSMQ etc. In a
    scenario, if we are using SOAP over HTTP, we are just utilizing HTTP as
    a transport. But HTTP is much more than just a transport. So, when we
    talk about REST architectural style, it dictates that “Instead of using
    complex mechanisms like CORBA, RPC or SOAP for communication, simply
    HTTP should be used for making calls”.

---
+++ ProjectX/MyWCFRestService/MyWCFRestService/ProductDataContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MyWCFRestService
+{
+    [DataContract]
+    public class ProductDataContract
+    {
+        [DataMember]
+        public int ProductId { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string CategoryName { get; set; }
+        [DataMember]
+        public int Price { get; set; }
+    }
+}
---
+++ ProjectX/MyWCFRestService/MyWCFRestService/Products.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWCFRestService
+{
+    public partial class Products
+    {
+        private static readonly Products _instance = new Products();
+
+        private List<ProductDataContract> products = new List<ProductDataContract>()
+        {
+            new ProductDataContract() { ProductId = 1, Name = "Product 1", CategoryName = "Category 1", Price = 10 },
+            new ProductDataContract() { ProductId = 1, Name = "Product 2", CategoryName = "Category 2", Price = 15 },
+            new ProductDataContract() { ProductId = 1, Name = "Product 3", CategoryName = "Category 3", Price = 20 },
+            new ProductDataContract() { ProductId = 1, Name = "Product 4", CategoryName = "Category 4", Price = 25 },
+            new ProductDataContract() { ProductId = 1, Name = "Product 5", CategoryName = "Category 5", Price = 30 },
+            new ProductDataContract() { ProductId = 1, Name = "Product 6", CategoryName = "Category 6", Price = 35 }
+        };
+
+        private Products()
+        {
+
+        }
+        public static Products Instance
+        {
+            get { return _instance; }
+        }
+
+        public List<ProductDataContract> ProductList
+        {
+            get { return products; }
+        }
+
+
+    }
+}
